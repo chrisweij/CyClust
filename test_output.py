822809f492793091df4c9824928b56b338ea7992
chrisweij <chris.weijenborg@wur.nl>
2024-06-13 12:54:31 +0200

    Read correct output file in testing

--- test_output.py
+++ test_output.py
@@ -1,6 +1,10 @@
 # content of test_sample.py
 import numpy as np
 import pytest
+import yaml
+
+with open("Options.yaml") as f:
+    Options = yaml.safe_load(f)
 
 @pytest.fixture(autouse=True)
 def change_test_dir(request, monkeypatch):
@@ -13,8 +17,13 @@ def test_cluster():
     #Rerun clustering
     exec(open("Find_clusters3d.py").read())
 
-    #Open new results
-    Results_test = np.load("Clusters_output/Winter_2011_2012_EI_1.0_tim_36.0_length_1.5_timlength_48.0_AlongTracksDirect_corrected.npz",allow_pickle=True)    
+    #Open new results    
+    formatter =  "{:1.1f}"
+    outfile = Options["outdir"] +  Options["str_result"] + formatter.format( Options["distthresh"]) +\
+    "_tim_" + formatter.format( Options["timthresh"]) + "_length_" + formatter.format( Options["lngthresh"]) +\
+    "_timlength_" + formatter.format( Options["timlngthresh"]*6.0) + "_" + Options["distmeth"] + "_corrected.npz"
+
+    Results_test = np.load(outfile,allow_pickle=True)    
 
     #Assert if they are the same
     assert all(Results_test["sorted_clusters"] == Results["sorted_clusters"])
