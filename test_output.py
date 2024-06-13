63f3639e959065a77fc7fe480f381d66e7ead764
chrisweij <chris.weijenborg@wur.nl>
2024-06-13 11:14:14 +0200

    Renamed correct array now for testing

--- test_output.py
+++ test_output.py
@@ -17,6 +17,6 @@ def test_cluster():
     Results_test = np.load("Clusters_output/Winter_2011_2012_EI_1.0_tim_36.0_length_1.5_timlength_48.0_AlongTracksDirect_corrected.npz",allow_pickle=True)    
 
     #Assert if they are the same
-    assert all(Results["sorted_clusters"] == Results_test["sorted_clusters"])
-    assert all(Results["sorted_subclusters_bjerknes"] == Results_test["sorted_subclusters_length"])
-    assert all(Results["sorted_subclusters_stagnant"] == Results_test["sorted_subclusters_nolength"])
+    assert all(Results_test["sorted_clusters"] == Results["sorted_clusters"])
+    assert all(Results_test["sorted_subclusters_bjerknes"] == Results["sorted_subclusters_length"])
+    assert all(Results_test["sorted_subclusters_stagnant"] == Results["sorted_subclusters_nolength"])
